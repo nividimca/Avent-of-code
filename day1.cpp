0571d9246e65e1a119d99b5e3b742f43327e91f1
Mihai Eni <KitiHaKera@support.ops>
2019-12-03 15:57:09 +0100

    day1 ok

--- day1.cpp
+++ day1.cpp
@@ -1,1 +1,17 @@
-#inlude<iostream>
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int myA[1000000];
+    int mass=0, temp=0;
+    while(cin>>mass){
+       myA[temp]=mass;
+        ++temp;
+    }
+    int fuel;
+    for(int i=0; i<temp; ++i){
+        fuel = fuel + ( (myA[i]/3) - 2 );        
+    }    
+    cout<<"Fuel for all mass = "<<fuel<<endl;
+}
