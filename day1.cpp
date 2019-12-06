763964765837966a8ded9cd258988a7f5c1a83ab
Mihai Eni <KitiHaKera@support.ops>
2019-12-06 15:41:44 +0100

    day1 Part2

--- day1.cpp
+++ day1.cpp
@@ -10,8 +10,22 @@ int main(){
         ++temp;
     }
     int fuel;
+    // Part1:
     for(int i=0; i<temp; ++i){
-        fuel = fuel + ( (myA[i]/3) - 2 );        
+        fuel += ( (myA[i]/3) - 2 );        
     }    
-    cout<<"Fuel for all mass = "<<fuel<<endl;
+    cout<<"Fuel for all mass Part 1= "<<fuel<<endl;
+    // Part2:
+    int newMass=0;
+    fuel=0;
+    for(int i=0; i<temp; ++i){
+        newMass= myA[i];
+        while(newMass/3 -2 >0){
+            int moduleFuel = (newMass/3 -2);
+            newMass = moduleFuel;
+            fuel += moduleFuel;
+        }    
+    } 
+    cout<<"Fuel for all mass Part 2= "<<fuel<<endl;
 }
+
